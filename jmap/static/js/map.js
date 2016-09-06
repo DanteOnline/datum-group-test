6ac90472f63799bd095fa6c49f2bcc9414819bfc
DanteOnline <iamdanteonline@gmail.com>
2016-09-06 20:47:32 +0300

    test geoJson

--- jmap/static/js/map.js
+++ jmap/static/js/map.js
@@ -1,9 +1,32 @@
+
+
 //Определяем карту, координаты центра и начальный масштаб
-var map = L.map('map').setView([56.326944, 44.0075], 12);
+var map = L.map('map').setView([56.326944, 44.0075], 6);
 
 //Добавляем на нашу карту слой OpenStreetMap
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a rel="nofollow" href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([56.326944, 44.0075]).addTo(map).bindPopup("<strong>КАРО ФИЛЬМ Шоколад</strong><br />Адрес: ул. Белинского, 124").openPopup();
+function onEachFeature(feature, layer) {
+    var popupContent = "<p>I started out as a GeoJSON " +
+            feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
+
+    if (feature.properties && feature.properties.popupContent) {
+        popupContent += feature.properties.popupContent;
+    }
+
+    layer.bindPopup(popupContent);
+}
+
+var testLayer = L.geoJson(test, {
+
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {icon: baseballIcon});
+    },
+
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+L.control.scale().addTo(map);
+
